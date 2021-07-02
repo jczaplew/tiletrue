624ea84e9c21d1ab88797445b87dc4ef8835643a
jczaplew <john@gaiagps.com>
2021-07-02 13:03:35 -0500

    Improve docs, add example

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # tiletrue
-Query a bunch of vector tiles at a given location
+Query many vector tiles at a given coordinate
+
+## API
+
+````typescript
+import TileTrue from 'tiletrue'
+
+const config = [{
+    name: 'Example',
+    url: 'https://domain.com/{z}/{x}/{y}.mvt',
+    zoom: 12,
+    layers: ['layer'],
+}]
+
+const tileTrue = TileTrue(config)
+
+(async () => {
+    const dataAtLocation = await tileTrue(-90, 45)
+
+    // returns an array of properties for each feature found at the location, in each layer, 
+    // of each source in the config. Each feature has the properties _layer and _sourceTileURL  
+    // that allows them to be related back to the config
+})
+
+````
 
 ## Development
 
--- src/utils/tileToGeoJSON.ts
+++ src/utils/tileToGeoJSON.ts
@@ -8,25 +8,25 @@ export default function tileToGeoJSON(buffer: any, tile: number[], layers: strin
 
     // handle zipped buffers
     if (buffer[0] === 0x78 && buffer[1] === 0x9C) {
-        buffer = zlib.inflateSync(buffer);
+        buffer = zlib.inflateSync(buffer)
     } else if (buffer[0] === 0x1F && buffer[1] === 0x8B) {
-        buffer = zlib.gunzipSync(buffer);
+        buffer = zlib.gunzipSync(buffer)
     }
 
-    const vectorTile = new vt.VectorTile(new Protobuf(buffer));
+    const vectorTile = new vt.VectorTile(new Protobuf(buffer))
     // Layers are available on the vector tile as a vectorTiles.layers Record<string, any> 
 
     const features: GeoJSON.Feature[] = []
     layers.forEach(function (layerID) {
-        const layer = vectorTile.layers[layerID];
+        const layer = vectorTile.layers[layerID]
         if (layer) {
-            for (var i = 0; i < layer.length; i++) {
-                const feature = layer.feature(i).toGeoJSON(tile[0], tile[1], tile[2]);
-                if (layers.length > 1) feature.properties.vt_layer = layerID;
-                features.push(feature);
+            for (let i = 0; i < layer.length; i++) {
+                const feature = layer.feature(i).toGeoJSON(tile[0], tile[1], tile[2])
+                feature.properties._layer = layerID
+                features.push(feature)
             }
         }   
-    });
+    })
 
     return features
 }
